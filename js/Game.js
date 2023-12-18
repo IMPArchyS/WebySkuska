b915746767f3edbc2e56398cf80208d28c7e96d7
DanielJanik <01djanik@gmail.com>
2023-12-18 17:02:13 +0100

    jumping cube

--- js/Game.js
+++ js/Game.js
@@ -1,44 +1,66 @@
-class Example extends Phaser.Scene
-{
-    preload ()
-    {
-        this.load.setBaseURL('https://labs.phaser.io');
+var characterX = 0; // Initial character position
+var characterY = 0; // Initial y position
+var characterYVelocity = 0; // Initial vertical velocity
+var onGround = true; // Is the cube on the ground?
+var gravity = 0.5; // Gravity
+var jumpForce = -15; // Jump force
 
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
-    }
+const KEY_A = 65;
+const KEY_D = 68;
 
-    create ()
-    {
-        this.add.image(400, 300, 'sky');
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('canvas-container');
+    background(200);
+    windowResized(); // Ensure correct canvas size at start
+    characterX = width / 2 - 25; // Middle of the screen horizontally
+    characterY = height - 50; // Bottom of the screen vertically
+}
 
-        const logo = this.physics.add.image(400, 100, 'logo');
+function draw() {
+    background(200);
+    drawCharacter(); // Draw the character at its current position
+    handleUserInput(); // Handle user input to move the character
+}
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
+function drawCharacter() {
+    // Draw the character as a simple rectangle
+    fill(255, 0, 0); // Red color
+    rect(characterX, characterY, 50, 50); // Character position and size
+    // Apply gravity
+    characterYVelocity += gravity;
 
-        particles.startFollow(logo);
+    // Apply vertical velocity to y position
+    characterY += characterYVelocity;
+
+    // Check if the cube is on the ground
+    if (characterY > height - 50) {
+        characterY = height - 50;
+        characterYVelocity = 0;
+        onGround = true;
     }
 }
 
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: Example,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
+function handleUserInput() {
+    if (keyIsDown(KEY_A)) { 
+        characterX -= 5; 
+        if (characterX + 50 < 0) { // If the character is completely off the left side of the screen
+            characterX = width; // Move it to the right side
+        }
+    } else if (keyIsDown(KEY_D)) { 
+        characterX += 5; 
+        if (characterX > width) { // If the character is completely off the right side of the screen
+            characterX = -50; // Move it to the left side
         }
     }
-};
+    if (onGround) { 
+        characterYVelocity = jumpForce;
+        onGround = false;
+    }
+}
 
-const game = new Phaser.Game(config);
+function windowResized() {
+    var containerWidth = select('#canvas-container').width;
+    resizeCanvas(containerWidth, windowHeight);
+    background(200);
+}
